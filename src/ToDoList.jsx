d700f6a1c55dc5850d541e766ee24bea3a66bc44
Kavindu Umayangana <161000554+kavinduUmayangana@users.noreply.github.com>
2024-05-31 11:10:02 +0530

    start the project

---
+++ src/ToDoList.jsx
@@ -0,0 +1,14 @@
+
+import React, { useState } from 'react'
+
+function ToDoList() {
+    const [tasks,setTasks]=useState([]);
+    const [newTask,setNewTask]=useState
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ToDoList
